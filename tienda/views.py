2fedef317c681d08cc85ea243a2848bb578e1fc3
HeidyAPrimo <heidyprimo10@outlook.com>
2024-01-10 18:48:56 -0500

    to see reflected the table of django into a DBs using the commands: py manage.py makemigrations and after py manage.py migrate, to see it's already reflected the table you should to create  a user with the next command: py manage.py createsuperuser and follow the steps, and running the server. After into de file admin.py let's register de (Producto) and to import. After let's crud, into the views.py and the def 'consultar' let´s import Producto and after, let´s to get the registers od products in the DBs with: producto= Producto.objects.all() y after adding a 3rd parameter as dictionary{}

--- tienda/views.py
+++ tienda/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from tienda.models import Productos
 
 # Create your views here.
 def home(request):
@@ -7,4 +8,7 @@ def home(request):
 #render(request,"prueba.html")
 
 def consulproducts(request):
-    return render(request, "productos.html")
+    productos= Productos.objects.all()
+    return render(request, "productos.html", {
+        'productos': productos
+    })
